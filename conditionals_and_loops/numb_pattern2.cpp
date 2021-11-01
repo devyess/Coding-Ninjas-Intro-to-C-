7b7b84c8d71cc49a1de08f71ecfdb19310668414
devyess <devyessbuchha@gmail.com>
2021-11-02 01:31:38 +0530

    numb_pattern2.cpp file modified

--- conditionals_and_loops/numb_pattern2.cpp
+++ conditionals_and_loops/numb_pattern2.cpp
@@ -1,24 +1,53 @@
-#include <iostream>
+/*
+Print the following pattern
+Pattern for N = 4
+...1
+..23
+.345
+4567
+The dots represent spaces.
+
+Input Format :
+N (Total no. of rows)
+
+Output Format :
+Pattern in N lines
+
+Sample Input :
+5
+
+Sample Output :
+....1
+...23
+..345
+.4567
+56789
+The dots represent spaces.
+----------------------------------------------------------------------------------------------
+*/
+#include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    int row=1;
-    int value=1;
-    while(row<=n){
-        int space=1;
-        while(space<=n-row){
-            cout<<" ";
-            space=space+1;
+    int N;
+    cin >> N;
+    int row = 1;
+    int value = 1;
+    
+    while(row <= N){
+        int space = 1;
+      
+        while(space <= N-row){
+        		cout << " ";
+        		space = space + 1;
+		}
+		int column = 1;
+        value = row;
+        while(column <= row){
+            cout << value;
+            column = column + 1;
+            value = value + 1;
         }
-        int column=1;
-        value=row;
-        while(column<=row){
-            cout<<value;
-            column++;
-            value++;
-        }
-    }
-    cout<<endl;
-    row++;
+        cout << endl;
+        row = row + 1;  
+	}
 }
