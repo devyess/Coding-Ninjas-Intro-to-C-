ac8ccb70e794e5ab16cda4404e264c71e65d5d79
devyess <devyessbuchha@gmail.com>
2021-11-02 16:40:09 +0530

    terms_AP.cpp file added

---
+++ operators_and_for_loops/terms_AP.cpp
@@ -0,0 +1,42 @@
+/*
+Write a program to print first x terms of the series 3N + 2 which are not multiples of 4.
+
+Input format :
+Integer x
+
+Output format :
+Terms of series (separated by space)
+
+Constraints :
+1 <= x <= 1,000
+
+Sample Input 1 :
+10
+
+Sample Output 1 :
+5 11 14 17 23 26 29 35 38 41
+
+Sample Input 2 :
+4
+
+Sample Output 2 :
+5 11 14 17
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    int j=1,i=1;
+    while(j<=x){
+        int a=(3*i)+2;
+        if(a%4==0){
+            i++;
+            continue;
+        }
+        cout<<a<<" ";
+        j++;
+        i++;
+    }
+}
